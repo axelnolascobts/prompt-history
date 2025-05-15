ca82e9d98487428a1bcaff8dc27e1ba16a62c0ac
miltonmej <milton.mejia6553@alumnos.udg.mx>
2025-05-14 22:27:21 -0600

    clase car

---
+++ class practices/class_car.js
@@ -0,0 +1,40 @@
+class Car {
+  constructor(brand, model, year) {
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+  }
+
+  getCarInfo() {
+    return `Car: ${this.brand} ${this.model}, Year: ${this.year}`;
+  }
+
+  static compareYears(car1, car2) {
+    if (car1.year > car2.year) {
+      return `${car1.model} is newer than ${car2.model}`;
+    } else if (car1.year < car2.year) {
+      return `${car2.model} is newer than ${car1.model}`;
+    } else {
+      return `${car1.model} and ${car2.model} are from the same year`;
+    }
+  }
+}
+
+class ElectricCar extends Car {
+  constructor(brand, model, year, batteryLife) {
+    super(brand, model, year);
+    this.batteryLife = batteryLife;
+  }
+
+  getCarInfo() {
+    return `${super.getCarInfo()} - Battery Life: ${this.batteryLife} hours`;
+  }
+}
+
+const car1 = new Car('Toyota', 'Corolla', 2018);
+const car2 = new Car('Honda', 'Civic', 2020);
+console.log(car1.getCarInfo());
+console.log(Car.compareYears(car1, car2));
+
+const tesla = new ElectricCar('Tesla', 'Model S', 2022, 24);
+console.log(tesla.getCarInfo());
