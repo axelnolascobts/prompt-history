4380af8c159f61b8b6e35beb18eb61ca524bbeab
Milton <miltonmejiam@gmail.com>
2025-05-20 17:47:05 -0600

    practica get,post,delete

--- notes-api/index.js
+++ notes-api/index.js
@@ -4,6 +4,8 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 
 const PORT=3000;
+let notas = []; 
+const regex = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/
 
 app.use(express.json());
 
@@ -11,10 +13,46 @@ app.get(`/notes`, (req, res) => {
     fs.readFile(`notes.json`, `utf-8`, (err, data) =>{
         if (err) return res.status(500).json({error: `Error al leer`});
 
-        const notas = JSON.parse(data);
+        notas = JSON.parse(data);
         res.json(notas);
     })
 })
+
+
+        app.get(`/notes/:id`, (req, res) => {
+            
+            const ID = req.params.id;
+            if(regex.exec(ID)){
+                let nota = notas.find(n => n.id === ID);
+                if (nota){
+                    res.json(nota);
+                }else{
+                    res.status(404).json({error: `Error al buscar`})
+                }
+            }else{
+                const titulo = req.params.id;
+                let nota = notas.find(n => n.title === titulo);
+                if (nota){
+                    res.json(nota);
+                }else{
+                    res.status(404).json({error: `Error al buscar`})
+            }
+            }
+            });
+    
+                app.delete(`/notes/:id`, (req, res) => {
+                    const ID = req.params.id;
+                    let index =  notas.findIndex(n => n.id === ID);
+                        if (index !== -1){
+                            const delete_nota = notas.splice(index,1);
+                            fs.writeFile('notes.json', JSON.stringify(notas, null, 3), (err) =>{
+                                if (err){ return res.status(500).json({ error: 'Error borrando nota' })};
+                                res.json(delete_nota);
+                        })
+                    }
+                });
+
+
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
