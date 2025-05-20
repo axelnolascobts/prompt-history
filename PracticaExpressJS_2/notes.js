6282a725719445383d02021d8f3c14b30bfb7a98
Isaac-Gallo <isaac.chacg20@gmail.com>
2025-05-20 17:48:10 -0600

    API pulida y funionando ahora si

--- PracticaExpressJS_2/notes.js
+++ PracticaExpressJS_2/notes.js
@@ -7,17 +7,17 @@ const fs = require('fs');
 
 app.use(express.json());
 
-const NOTES_FILE = "notes.json"
+const NotesFiles = "notes.json"
 
 let notes = [];
 
 //console.log(uuidv4());
 
-fs.readFile(NOTES_FILE, 'utf8', function(err, data) {
+fs.readFile(NotesFiles, 'utf8', function(err, data) {
 
     if (err) {
 
-        fs.appendFile(NOTES_FILE, '{"notes": []}', function (err) {
+        fs.appendFile(NotesFiles, '{"notes": []}', function (err) {
 
             if (err) {
                 //console.error("Error creating to file");
@@ -31,26 +31,42 @@ fs.readFile(NOTES_FILE, 'utf8', function(err, data) {
 
     //console.log(data);
     notes = JSON.parse(data).notes;
+    
 });
 
 app.get('/notes', (req, res) => {
     res.json(notes);
+    
 });
 
-app.get('/notes/:id', (req, res) => {
-    const ID = parseInt(req.params.id);
-    const NOTE = notes.find(n => n.id === ID);
+app.get('/notes/:value', (req, res) => {
+    let id = req.params.value; 
+    let note = notes.find(n => n.id ===id);
 
-    if (NOTE) {
-        res.json(NOTE);
+    if (note) {
+
+        res.json(note);
+
     } else {
-        res.status(404).json({ message: "Note not found" });
+
+        let title = req.params.value;
+        note = notes.find(n => n.title === title);
+        
+        if (note) {
+
+            res.json(note);
+
+        } else {
+
+            res.status(404).json({ message: "Note not found" });
+        }
     }
 
 });
 
-app.get('/notes/:title', (req, res) => {
-    const TITLE = parseInt(req.params.title);
+/*app.get('/notes/title', (req, res) => {
+    const TITLE = req.query;
+    console.log(TITLE);
     const NOTE = notes.find(n => n.title === TITLE);
 
     if (NOTE) {
@@ -59,47 +75,47 @@ app.get('/notes/:title', (req, res) => {
         res.status(404).json({ message: "Note not found" });
     }
 
-});
+});*/
 
 app.post("/notes", (req, res) => {
 
-    const newNote = {
+    const NewNotes = {
         id: uuidv4(),
         title: req.body.title,
         content: req.body.content
     };
 
-    notes.push(newNote);
+    notes.push(NewNotes);
 
-    fs.writeFile(NOTES_FILE, JSON.stringify({ notes: notes }, null, 2), (err) => {
+    fs.writeFile(NotesFiles, JSON.stringify({ notes: notes }, null, 2), (err) => {
 
         if (err) {
             //console.error("Error writing to file");
             return res.status(500).json({ message: "Failed to save note" });
         }
 
-        res.status(201).json(newNote);
+        res.status(201).json(NewNotes);
     });
 
 });
 
 
-app.delete("/notes/:id", (req, res) => {
-    const ID = req.params.id;
+app.delete("/notes/:value", (req, res) => {
+    const Id = req.params.value;
 
-    const index = notes.findIndex(n => n.id === ID);
+    const Index = notes.findIndex(n => n.id ===Id);
 
-    if (index !== -1) {
-        const deletedNote = notes.splice(index, 1)[0];
+    if (Index !== -1) {
+        const DeletedNote = notes.splice(index, 1)[0];
 
-        fs.writeFile(NOTES_FILE, JSON.stringify({ notes: notes }, null, 2), (err) => {
+        fs.writeFile(NotesFiles, JSON.stringify({ notes: notes }, null, 2), (err) => {
 
             if (err) {
                 //console.error("Error writing to file");
                 return res.status(500).json({ message: "Failed to delete note" });
             }
 
-            res.json({ message: "Note deleted", note: deletedNote });
+            res.json({ message: "Note deleted", note: DeletedNote });
         });
     } else {
         res.status(404).json({ message: "Note not found" });
