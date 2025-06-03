868a714c5934c42695b252449331921a72197ba8
Isaac-Gallo <isaac.chacg20@gmail.com>
2025-06-03 17:59:10 -0600

    Avance socket

--- ServerNodeSocket/client.js
+++ ServerNodeSocket/client.js
@@ -1,37 +1,64 @@
 const MESSAGE_INPUT = document.getElementById("message-input");
 const SEND_BUTTON = document.getElementById("send-button");
 const CHAT_MESSAGES = document.getElementById("chat-messages");
+const SHOW_USERNAME = document.getElementById("show-user-id");
 
-const SOCKET = io();
+
 let userName = "";
+let socket = null;
 
-while(userName.length >= 30 || userName.length < 1 || userName === null) {
 
-    userName = prompt("INSERT USER NAME (no more 30 charcaters)");
+function askUserName() {
+
+    while(userName === null || userName.length >= 30 || userName.trim().length < 1) {
+
+        userName = prompt("INSERT USER NAME (no more 30 charcaters)");
         
-}
-
-SOCKET.on('connect', () => {
-
-    console.log('conected to server');
-
-    SOCKET.emit("chat message", `User ${userName} connected to server`);
-
-});
-
-SOCKET.on('chat message', (message) => {
-
-    const MESSAGE = document.createElement("li");
-    MESSAGE.textContent = message;
-    CHAT_MESSAGES.appendChild(MESSAGE);
-
-});
-
-SEND_BUTTON.onclick = () => {
-
-    if(MESSAGE_INPUT.value !== ""){
-
-        SOCKET.emit("chat message", `[ ${userName} ]: ${MESSAGE_INPUT.value}`);
-        MESSAGE_INPUT.value = "";
     }
+
+    socket = io();
+
+    socket.on('connect', () => {
+
+        //console.log('conected to server');
+
+        socket.emit('set username', userName);
+        SHOW_USERNAME.textContent = userName;
+
+    });
+
+    socket.on('userName error', (message) => {
+
+        alert(message);
+        userName = "";
+        askUserName();
+
+    });
+
+    socket.on('chat message', (message) => {
+
+        const MESSAGE = document.createElement("li");
+        //MESSAGE.textContent = message;
+        CHAT_MESSAGES.appendChild(MESSAGE);
+        const MESSAGE_CONTENT = document.createElement("a");
+        MESSAGE_CONTENT.textContent = message;
+        MESSAGE.appendChild(MESSAGE_CONTENT);
+        const DELETE_BUTTON = document.createElement("p");
+        DELETE_BUTTON.className("delete-button");
+        DELETE_BUTTON.textContent = "DELETE MESSAGE";
+        MESSAGE.appendChild(DELETE_BUTTON);
+
+    });
+
+    SEND_BUTTON.onclick = () => {
+
+        if(MESSAGE_INPUT.value.trim() !== ""){
+
+            socket.emit("chat message", MESSAGE_INPUT.value);
+            MESSAGE_INPUT.value = "";
+        }
+    }
+
 }
+
+askUserName();
--- ServerNodeSocket/server.js
+++ ServerNodeSocket/server.js
@@ -7,24 +7,63 @@ const SERVER = createServer(APP);
 const IO = new Server(SERVER);
 const PORT = 3000;
 
-APP.use(express.static('//home/user/Documentos/prompt-history/ServerNodeSocket/'));
+APP.use(express.static('/home/user/Documentos/prompt-history/ServerNodeSocket/'));
+let userNames = [];
 
 IO.on('connection', (socket) => {
 
-    console.log('client connected');
+    //console.log('client connected');
+
+    socket.on('set username', (userName) => {
+
+        let userNameExist = userNames.filter((userNameInList) => userNameInList === userName);
+        
+
+        if(userNameExist.length === 0){
+
+            socket.userName = userName;
+            userNames.push(userName);
+
+            console.log(`User: ${socket.userName} with id: ${socket.id} connected to server`);
+
+            IO.emit('chat message', `User: ${socket.userName} connected to server`);
+
+        } else {
+
+            socket.emit('userName error', "These username is occupied");
+            socket.disconnect();
+
+        }
+ 
+    });
 
     socket.on('chat message', (message) => {
 
-        console.log(`Message recived: ${message}`);
-        IO.emit('chat message', message);
+        console.log(`Message "${message}" recived from user: ${socket.userName}, with id: ${socket.id}`);
+
+        IO.emit('chat message', `[${socket.userName}]: ${message}`);
 
     });
 
+
     socket.on('disconnect', () => {
 
-        console.log('client disconnected');
-        IO.emit('chat message', `An User Was disconnected`);
+        if(socket.userName !== undefined) {
 
+            console.log(`User: ${socket.userName} with id: ${socket.id} was discconected`);
+
+            IO.emit('chat message', `User: ${socket.userName} was discconected`);
+
+            let userNameIndex = userNames.indexOf(socket.userName);
+
+            if(userNameIndex !== -1) {
+
+                userNames.splice(userNameIndex, 1);
+
+            }
+
+        }
+        
     });
     
 });
