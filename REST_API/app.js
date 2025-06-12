1f30a5d0482fe7f8d822d0cfaf0acb5947543d3b
Isaac-Gallo <isaac.chacg20@gmail.com>
2025-06-11 23:36:20 -0600

    rest-api con swagger

--- REST_API/app.js
+++ REST_API/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const SWAGGER_UI = require('swagger-ui-express');
+const SWAGGER_DOCUMENT = require('./api-docs/swagger.json');
 const APP = express();
 const ROUTE = require('./routes/routes.js');
 const PORT = 3000;
@@ -6,6 +8,8 @@ const PORT = 3000;
 APP.use(express.json());
 
 APP.use('/students', ROUTE);
+
+APP.use('/api-docs', SWAGGER_UI.serve, SWAGGER_UI.setup(SWAGGER_DOCUMENT));
 
 APP.use((request, response) => {
     response.status(404).json({ message: "Route not found" });
